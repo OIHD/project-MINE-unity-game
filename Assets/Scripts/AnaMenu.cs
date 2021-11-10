f727f9be6be30965ee41189572354940dad27504
Göktuğ <oihdofficial@outlook.com>
2021-11-11 02:13:17 +0300

    project MAYIN V1.1.5

---
+++ Assets/Scripts/AnaMenu.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnaMenu : MonoBehaviour
+{
+    public AudioSource baslamaSES;
+    public void OyunuBaslat ()
+    {
+        baslamaSES.Play();
+        StartCoroutine(oyunubaslat());
+    }
+
+        IEnumerator oyunubaslat()
+    {
+        yield return new WaitForSeconds(1);
+        string SonrakiSeviye = "bolum1" ;
+        SceneManager.LoadScene(SonrakiSeviye.ToString());
+    }
+
+}
